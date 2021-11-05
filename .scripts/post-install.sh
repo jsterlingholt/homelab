d3ea9b3322e8fd3b939a423c20fd50b67b1e500c
jsterlingholt@me.com <sterling@sterlings-MacBook-Air.local>
2021-11-04 23:05:53 -0500

    download iso image

--- .scripts/post-install.sh
+++ .scripts/post-install.sh
@@ -6,5 +6,5 @@ echo 'hello'
 # download Ubuntu Server
 rm -rf ubuntu.iso
 cd /var/lib/vz/template/iso
-wget https://releases.ubuntu.com/20.04.3/ubuntu-20.04.3-live-server-amd64.iso?_ga=2.165848378.522787571.1636046166-1234176970.1634323943
-mv ubuntu* ubuntu.iso
+wget https://releases.ubuntu.com/20.04.3/ubuntu-20.04.3-live-server-amd64.iso
+mv ubuntu*.iso ubuntu.iso
